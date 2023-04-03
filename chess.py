afb9f94724d2e3271b0ccd6672cb87f689dfa580
oskarihei <oskari.heinonen@tuni.fi>
2023-04-03 18:08:32 +0300

    implemented chessboard gui into the Gui class implemented support for FEN strings to be loaded implemented a print_board function for debugging

--- chess.py
+++ chess.py
@@ -6,39 +6,55 @@ Opiskelijanumero: 151242115
 Chess
 """
 
+
 import tkinter as tk
 
 class Gui:
     """
     """
     def __init__(self):
-        pass
+        """Initializes the gui-object with an empty chessboard
+        and a menu.
+        """
+        self.__mainwindow = tk.Tk()
+        self.__buttons = []
+        for x in range(1,9):
+            self.__row = []
+            for y in range(1,9):
+                self.__button = tk.Button(self.__mainwindow,\
+                    width=10, height=5, borderwidth=1)
+                self.__button.grid(row=x, column=y)
+                if x % 2 == 0 and y % 2 != 0:
+                    self.__button.config(bg="brown")
+                elif x % 2 != 0 and y % 2 == 0:
+                    self.__button.config(bg="brown")
+                self.__row.append(self.__button)
+            self.__buttons.append(self.__row)
+
+        self.__menubar = tk.Menu(self.__mainwindow)
+        self.__file_menu = tk.Menu(self.__menubar)
+        self.__file_menu.add_command(label="Load position")
+        self.__menubar.add_cascade(menu=self.__file_menu, label="File")
+        self.__mainwindow.config(menu=self.__menubar)
+        self.__mainwindow.mainloop()        
+
+
+    def set_position(self, board):
+        """Sets the pieces to the correct places given by 
+        the <board> parameter.
+
+        :param board: list, pieces in the data structure
+        """
         
+
 
 
 class Piece:
     """
     """
-    def __init__(self, category, color, position):
+    def __init__(self, category, color):
         self.__category = category
         self.__color = color
-        self.__position = position
-        self.__alive = True
-
-    def move(self, target):
-        """
-        """
-
-    def die(self):
-        """
-        """
-        self.__alive = False
-
-    def get_position(self):
-        """
-        maybe tuple
-        """
-        return self.__position
 
     def get_category(self):
         """
@@ -49,28 +65,11 @@ class Piece:
         """
         """
         return self.__color
-
-    def attacked_squares(self):
+    
+    def __str__(self):
         """
         """
-        if self.__category == "pawn":
-            pass
-
-        elif self.__category == "knight":
-            pass
-
-        elif self.__category == "bishop":
-            pass
-
-        elif self.__category == "rook":
-            pass
-
-        elif self.__category == "queen":
-            pass
-
-        elif self.__category == "king":
-            pass
-
+        return f"{self.__color} {self.__category}"
 
 
 
@@ -86,10 +85,14 @@ board = [
         [None,None,None,None,None,None,None,None]
         ]
 
-def threatened_pieces(piece):
+def threatened_pieces(piece, board):
     """Returns a list of pieces which are threathened by the parameter piece.
     """
+    return
     
+def legal_moves(piece, board):
+    return
+
 
 def in_check(piece):
     """
@@ -100,16 +103,65 @@ def in_check(piece):
     else:
         return None
 
+START_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 
-def initialze_game(board):
-    """Puts all the pieces into their starting positions.
+# Lowercase = black , uppercase = white
+pieces = {
+        "p": Piece("pawn", "b"), "P": Piece("pawn", "w"),
+        "n": Piece("knight", "b"), "N": Piece("knight", "w"),
+        "b": Piece("bishop", "b"), "B": Piece("bishop", "w"),
+        "r": Piece("rook", "b"), "R": Piece("rook", "w"),
+        "q": Piece("queen", "b"), "Q": Piece("queen", "w"),
+        "k": Piece("king", "b"), "K": Piece("king", "w")
+        }
+
+
+def load_position(board, pieces, fen_string):
+    row = 0
+    column = 0
+    while row < 8:
+        for char in fen_string:
+
+            if char == "/":
+                column = 0
+                row += 1
+        
+            elif column == 8:
+                column = 0
+                row += 1
+            
+            elif row == 8:
+                break
+
+            elif char.isnumeric():
+                column += int(char)
+
+            else:
+                print(row)
+
+                board[row][column] = pieces[char]
+                column += 1
+
+
+def print_board(board):
+    """for debug purposes
     """
-    for square in board[1]:
-        square = Piece("pawn")
+    load_position(board, pieces, START_POSITION)
+    count = 0
+    for row in board:
+        for piece in row:
+            if count == 7:
+                print("{:<8}".format(str(piece)))
+                
+                count = 0
+            else:
+                print("{:<8}".format(str(piece)), end=" ")
+                count += 1
 
 def main():
-   
-    return
+    load_position(board, pieces, START_POSITION)
+    print_board(board)
+    gui = Gui()
 
 if __name__ == '__main__':
     main()
